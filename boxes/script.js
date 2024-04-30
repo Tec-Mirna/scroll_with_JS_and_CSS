aa022e0abdf5d73a8f3e94efb021f9e419374c35
Mirna <mirnarecinos382@gmail.com>
2024-04-29 23:24:37 -0300

    Modificaciones

--- boxes/script.js
+++ boxes/script.js
@@ -3,15 +3,16 @@ const btn = document.getElementById('btn')
 
 btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
+//Creacion de las cajas
 function createBoxes() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-      const box = document.createElement('div')
-      box.classList.add('box')
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
-      boxesContainer.appendChild(box)
+  for (let i = 0; i < 4; i++) { //filas
+    for (let j = 0; j < 4; j++) { // columnas
+      const box = document.createElement('div') //creo elemento (caja)
+      box.classList.add('box') //Agrego la clase box
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px` //Posicion 
+      boxesContainer.appendChild(box) //agregar la caja al cont.
     }
   }
 }
 
-createBoxes()
+createBoxes() //Llamado a la función para crear las cajas
